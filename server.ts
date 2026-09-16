d87407ee3db32e9e97cfc0300522fe369c3372a9
Wagner Brammer <wagbrammer@gmail.com>
2026-09-16 16:54:19 -0300

    fix: remove invalid chat.messages.readonly scope, use chat.spaces.readonly for both spaces and messages

--- server.ts
+++ server.ts
@@ -816,8 +816,7 @@ async function startServer() {
       'https://www.googleapis.com/auth/contacts.readonly',     // Ler contatos do Google
       'https://www.googleapis.com/auth/contacts',              // Ler e escrever contatos
       'https://www.googleapis.com/auth/chat.messages.create',  // Enviar mensagens no Google Chat
-      'https://www.googleapis.com/auth/chat.messages.readonly', // Ler mensagens do Google Chat
-      'https://www.googleapis.com/auth/chat.spaces.readonly',  // Listar espaços/salas
+      'https://www.googleapis.com/auth/chat.spaces.readonly',  // Listar espaços/salas e mensagens
       'https://www.googleapis.com/auth/gmail.readonly',
       'https://www.googleapis.com/auth/drive.readonly'
     ];
